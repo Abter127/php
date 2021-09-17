0a18108f07df4eff355c546c9bb2cb3efe4be101
Abter singh <88001393+Abter127@users.noreply.github.com>
2021-09-17 19:11:39 +0530

    done

---
+++ Restaurant-Reservation/footer.php
@@ -0,0 +1,22 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="https://www.facebook.com/shudhrestaurant/"><i class="fa fa-facebook"></i></a>
+        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        
+      </div>
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>Shudh Food</b></p>
+        <p><i class="fa fa-phone"></i> +00 (123) 456 7890 &nbsp;<i class="fa fa-envelope-o"></i> info@domain.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+<!--end of footer-->
+</body>
+<!---end of body-->
+</html>
